f276a6f31b5a70502fa26756260a418b27e4fd74
imshivendu <shivendujha11@gmail.com>
2021-08-11 15:36:55 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+#Load Iris.csv into a pandas dataFrame.
+iris = pd.read_csv("iris.csv")
+print(iris.head())
